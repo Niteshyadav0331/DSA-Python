07567b120aafca9a5db9b77fbb2d2cfc47547d82
Nitesh Yadav <55007875+Niteshyadav0331@users.noreply.github.com>
2022-07-08 07:45:06 +0530

    Update insertion.py

--- Linked List/insertion.py
+++ Linked List/insertion.py
@@ -3,7 +3,6 @@ class Node:
         self.data = data
         self.next = None
 
-
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -14,14 +13,12 @@ class LinkedList:
 
         self.head = new_node
 
-
     def insertLast(self, data):
         new_node = Node(data)
         new_node.next = None
 
         if self.head is None:
             self.head = new_node
-            return
 
         temp = self.head
 
@@ -35,46 +32,45 @@ class LinkedList:
         if pos < 1 and pos > size:
             print(f"Can't insert {pos} is not a valid position")
 
+        elif pos == 0:
+            return self.insertStart(data)
+
         else:
             temp = self.head
             new_node = Node(data)
 
         for i in range(1, pos):
             temp = temp.next
-            pos -= 1
 
         new_node.next = temp.next
         temp.next = new_node
 
-    def display(self):
+    def calcSize(self):
+        size = 0
         temp = self.head
 
-        while temp:
+        while temp is not None:
+            temp = temp.next
+            size += 1
+        return size
+
+    def display(self):
+        if self.head is None:
+            return
+
+        temp = self.head
+
+        while temp is not None:
             print(temp.data, end = " ")
             temp = temp.next
 
-    def calcSize(self):
-        size = 0
-
-        node = self.head
-
-        while node is not None:
-            node = node.next
-            size += 1
-
-        return size
-
-
 ll = LinkedList()
+ll.insertStart(6)
+ll.insertStart(8)
+ll.insertStart(10)
 ll.insertStart(12)
-ll.insertStart(15)
-ll.insertStart(17)
-
-ll.insertLast(31)
-ll.insertLast(11)
-ll.insertLast(21)
-
-ll.insertPosition(2, 23)
-
+ll.insertLast(14)
+ll.insertLast(16)
+ll.insertPosition(2, 5)
 
 ll.display()
